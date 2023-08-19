41b3695d9be6ba112581cedc9efd0ed57972be26
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-08-19 00:52:33 -0500

    finished product

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 ## Description
 
-I was tasked with creating a working quiz with questions about coding with a timer function and a fully operational high scores list.
+I was tasked with creating a working quiz with questions about coding with a timer function and a fully operational high scores list. I used arrays, functions, and event viewers to allow the user to click on the answers of each question while affecting the timer and letting you know if the answer was correct or not.
 
 ## Installation
 
-I used arrays, functions, and event viewers to allow the user to click on the answers of each question while affecting the timer and letting you know if the answer was correct or not.
+No installation necessary but you can clone the [repository](https://github.com/MrMessyFace/coding-quiz-challenge) from GitHub to edit it and make your own version.
 
 ## Usage
 
-To use this page, you would click the start button. Five questions would appear one after the other in a randomized order. A timer would start counting down from 50. Each correct answer would display as correct and add 10 seconds to the timer. Each wrong answer would display as wrong and subtract 10 seconds to the timer. When finished, the remaining time is your score and you are then able to input your initials for the high score list. The high score list is obtainable at any point and has a clear list option.
+To use this page, you would click the start button. Five questions would appear one after the other in a randomized order. A timer would start counting down from 50. Each correct answer would display as correct and add 10 seconds to the timer. Each wrong answer would display as wrong and subtract 10 seconds to the timer. When finished, the remaining time is your score and you are then able to input your initials for the high score list. The high score list is obtainable at any point and has a list clearing option.
 
-![Screenshot](assets/images/mrmessyface.github.io_04-coding-quiz-challenge_.png)
+![Screenshot](assets/images/coding-quiz-challenge-screenshot.jpg)
 
 ## Credits
 
-HTML, CSS, and JavaScript code were originally used by [Achigas](https://github.com/Achigas/CodeQuiz-Challenge4) but were polished by me with changes made to the colors, the questions, button and font sizes, and functions to better fit the requirements of the assignment.
+HTML, CSS, and JavaScript code were originally used by [Achigas](https://github.com/Achigas/CodeQuiz-Challenge4) but were polished by me with changes made to the colors, the questions, button and font sizes, and functions to better fit the requirements of the assignment from the Northwestern University Full Stack Coding Bootcamp.
 
-https://mrmessyface.github.io/coding-quiz-challenge/
+## Helpful Links
+
+- [Repository](https://github.com/MrMessyFace/coding-quiz-challenge)
+- [Live App](https://mrmessyface.github.io/coding-quiz-challenge/)
+
+&copy; 2023 Jessie S. Galindo. All Rights Reserved.
--- assets/js/script.js
+++ assets/js/script.js
@@ -34,267 +34,298 @@ var arrayShuffledQuestions;
 var questionIndex = 0;
 
 var questions = [
-    {
-        q: 'Commonly used data types DO NOT include:',
-        a: 'alerts',
-        choices: [{ choice: 'strings' }, { choice: 'booleans' }, { choice: 'alerts' }, { choice: 'numbers' }]
-    },
-    {
-        q: 'The condition in an if / else statement is enclosed within _____.',
-        a: 'curly brackets',
-        choices: [{ choice: 'quotes' }, { choice: 'curly brackets' }, { choice: 'parentheses' }, { choice: 'square brackets' }]
-    },
-    {
-        q: 'Arrays in JavaScript can be used to store _____.',
-        a: 'all of the above',
-        choices: [{ choice: 'numbers and strings' }, { choice: 'other arrays' }, { choice: 'booleans' }, { choice: 'all of the above' }]
-    },
-    {
-        q: 'String values must be enclosed within _____ when being assigned to variables.',
-        a: 'quotes',
-        choices: [{ choice: 'commas' }, { choice: 'curly brackets' }, { choice: 'quotes' }, { choice: 'parentheses' }]
-    },
-    {
-        q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-        a: 'console.log',
-        choices: [{ choice: 'JavaScript' }, { choice: 'terminal / bash' }, { choice: 'for loops' }, { choice: 'console.log' }]
-    },
+  {
+    q: "Commonly used data types DO NOT include:",
+    a: "alerts",
+    choices: [
+      { choice: "strings" },
+      { choice: "booleans" },
+      { choice: "alerts" },
+      { choice: "numbers" },
+    ],
+  },
+  {
+    q: "The condition in an if / else statement is enclosed within _____.",
+    a: "curly brackets",
+    choices: [
+      { choice: "quotes" },
+      { choice: "curly brackets" },
+      { choice: "parentheses" },
+      { choice: "square brackets" },
+    ],
+  },
+  {
+    q: "Arrays in JavaScript can be used to store _____.",
+    a: "all of the above",
+    choices: [
+      { choice: "numbers and strings" },
+      { choice: "other arrays" },
+      { choice: "booleans" },
+      { choice: "all of the above" },
+    ],
+  },
+  {
+    q: "String values must be enclosed within _____ when being assigned to variables.",
+    a: "quotes",
+    choices: [
+      { choice: "commas" },
+      { choice: "curly brackets" },
+      { choice: "quotes" },
+      { choice: "parentheses" },
+    ],
+  },
+  {
+    q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: "console.log",
+    choices: [
+      { choice: "JavaScript" },
+      { choice: "terminal / bash" },
+      { choice: "for loops" },
+      { choice: "console.log" },
+    ],
+  },
 ];
 
 // Starting Function
 var renderStartPage = function () {
-    containerHighScoresEl.classList.add("hide");
-    containerHighScoresEl.classList.remove("show");
-    containerStartEl.classList.remove("hide");
-    containerStartEl.classList.add("show");
-    containerScoreEl.removeChild(containerScoreEl.lastChild);
-    questionIndex = 0;
-    gameover = "";
-    timerEl.textContent = 0;
-    score = 0;
+  containerHighScoresEl.classList.add("hide");
+  containerHighScoresEl.classList.remove("show");
+  containerStartEl.classList.remove("hide");
+  containerStartEl.classList.add("show");
+  containerScoreEl.removeChild(containerScoreEl.lastChild);
+  questionIndex = 0;
+  gameover = "";
+  timerEl.textContent = 0;
+  score = 0;
 
-    if (correctEl.className = "show") {
-        correctEl.classList.remove("show");
-        correctEl.classList.add("hide")
-    };
+  if ((correctEl.className = "show")) {
+    correctEl.classList.remove("show");
+    correctEl.classList.add("hide");
+  }
 
-    if (wrongEl.className = "show") {
-        wrongEl.classList.remove("show");
-        wrongEl.classList.add("hide");
-    };
+  if ((wrongEl.className = "show")) {
+    wrongEl.classList.remove("show");
+    wrongEl.classList.add("hide");
+  }
 };
 
 // Timer Function
 var setTime = function () {
-    timeLeft = 50;
+  timeLeft = 50;
 
-    var timerCheck = setInterval(function () {
-        timerEl.innerText = timeLeft;
-        timeLeft--;
+  var timerCheck = setInterval(function () {
+    timerEl.innerText = timeLeft;
+    timeLeft--;
 
-        if (gameover) {
-            clearInterval(timerCheck);
-        };
+    if (gameover) {
+      clearInterval(timerCheck);
+    }
 
-        if (timeLeft < 0) {
-            showScore();
-            timerEl.innerText = 0;
-            clearInterval(timerCheck);
-        };
-    }, 1000);
+    if (timeLeft < 0) {
+      showScore();
+      timerEl.innerText = 0;
+      clearInterval(timerCheck);
+    }
+  }, 1000);
 };
 
 // Start Quiz Function
 var startGame = function () {
-    containerStartEl.classList.add('hide');
-    containerStartEl.classList.remove('show');
-    containerQuestionEl.classList.remove('hide');
-    containerQuestionEl.classList.add('show');
-    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    setTime();
-    setQuestion();
+  containerStartEl.classList.add("hide");
+  containerStartEl.classList.remove("show");
+  containerQuestionEl.classList.remove("hide");
+  containerQuestionEl.classList.add("show");
+  arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  setTime();
+  setQuestion();
 };
 
 // Next Question Function
 var setQuestion = function () {
-    resetAnswers();
-    displayQuestion(arrayShuffledQuestions[questionIndex]);
+  resetAnswers();
+  displayQuestion(arrayShuffledQuestions[questionIndex]);
 };
 
 // Clear Answers Function
 var resetAnswers = function () {
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
-    };
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  }
 };
 
 // Display Question Function
 var displayQuestion = function (index) {
-    questionEl.innerText = index.q
-    for (var i = 0; i < index.choices.length; i++) {
-        var answerButton = document.createElement('button');
-        answerButton.innerText = index.choices[i].choice;
-        answerButton.classList.add('btn');
-        answerButton.classList.add('answerbtn');
-        answerButton.addEventListener("click", answerCheck);
-        answerButtonsEl.appendChild(answerButton);
-    };
+  questionEl.innerText = index.q;
+  for (var i = 0; i < index.choices.length; i++) {
+    var answerButton = document.createElement("button");
+    answerButton.innerText = index.choices[i].choice;
+    answerButton.classList.add("btn");
+    answerButton.classList.add("answerbtn");
+    answerButton.addEventListener("click", answerCheck);
+    answerButtonsEl.appendChild(answerButton);
+  }
 };
 
 // Display if Correct
 var answerCorrect = function () {
-    if (correctEl.className = "hide") {
-        correctEl.classList.remove("hide");
-        correctEl.classList.add("banner");
-        wrongEl.classList.remove("banner");
-        wrongEl.classList.add("hide");
-    };
+  if ((correctEl.className = "hide")) {
+    correctEl.classList.remove("hide");
+    correctEl.classList.add("banner");
+    wrongEl.classList.remove("banner");
+    wrongEl.classList.add("hide");
+  }
 };
 
 // Display if Wrong
 var answerWrong = function () {
-    if (wrongEl.className = "hide") {
-        wrongEl.classList.remove("hide");
-        wrongEl.classList.add("banner");
-        correctEl.classList.remove("banner");
-        correctEl.classList.add("hide");
-    };
+  if ((wrongEl.className = "hide")) {
+    wrongEl.classList.remove("hide");
+    wrongEl.classList.add("banner");
+    correctEl.classList.remove("banner");
+    correctEl.classList.add("hide");
+  }
 };
 
 // Check if Answer is Correct or Wrong
 var answerCheck = function (event) {
-    var selectedAnswer = event.target;
-    if (arrayShuffledQuestions[questionIndex].a === selectedAnswer.innerText) {
-        answerCorrect();
-        timeLeft = timeLeft + 10;
-    } else {
-        answerWrong();
-        timeLeft = timeLeft - 10;
-    };
+  var selectedAnswer = event.target;
+  if (arrayShuffledQuestions[questionIndex].a === selectedAnswer.innerText) {
+    answerCorrect();
+    timeLeft = timeLeft + 10;
+  } else {
+    answerWrong();
+    timeLeft = timeLeft - 10;
+  }
 
-    // Check if There are More Questions
-    questionIndex++;
-    if (arrayShuffledQuestions.length > questionIndex) {
-        setQuestion();
-    } else {
-        gameover = "true";
-        showScore();
-    };
+  // Check if There are More Questions
+  questionIndex++;
+  if (arrayShuffledQuestions.length > questionIndex) {
+    setQuestion();
+  } else {
+    gameover = "true";
+    showScore();
+  }
 };
 
 // Display Score Function
 var showScore = function () {
-    containerQuestionEl.classList.add("hide");
-    containerEndEl.classList.remove("hide");
-    containerEndEl.classList.add("show");
-    score = timeLeft;
-    var scoreDisplay = document.createElement("p");
-    scoreDisplay.innerText = ("Your final score is " + score + "!");
-    containerScoreEl.appendChild(scoreDisplay);
+  containerQuestionEl.classList.add("hide");
+  containerEndEl.classList.remove("hide");
+  containerEndEl.classList.add("show");
+  score = timeLeft;
+  var scoreDisplay = document.createElement("p");
+  scoreDisplay.innerText = "Your final score is " + score + "!";
+  containerScoreEl.appendChild(scoreDisplay);
 };
 
 // Set High Score Function
 var createHighScore = function (event) {
-    event.preventDefault();
-    var initials = document.querySelector("#initials").value;
-    if (!initials) {
-        alert("Enter your intials!");
-        return;
-    };
+  event.preventDefault();
+  var initials = document.querySelector("#initials").value;
+  if (!initials) {
+    alert("Enter your intials!");
+    return;
+  }
 
-    formInitials.reset();
+  formInitials.reset();
 
-    var highScore = {
-        initials: initials,
-        score: score
-    };
+  var highScore = {
+    initials: initials,
+    score: score,
+  };
 
-    // Push and Sort High Scores
-    highScores.push(highScore);
-    highScores.sort((a, b) => { return b.score - a.score });
+  // Push and Sort High Scores
+  highScores.push(highScore);
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
 
-    // Clear High Score List
-    while (listHighScoreEl.firstChild) {
-        listHighScoreEl.removeChild(listHighScoreEl.firstChild);
-    };
+  // Clear High Score List
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
 
-    // Create List Element for High Scores
-    for (var i = 0; i < highScores.length; i++) {
-        var highScoreEl = document.createElement("li");
-        highScoreEl.className = "high-score";
-        highScoreEl.innerHTML = highScores[i].initials + " - " + highScores[i].score;
-        listHighScoreEl.appendChild(highScoreEl);
-    };
+  // Create List Element for High Scores
+  for (var i = 0; i < highScores.length; i++) {
+    var highScoreEl = document.createElement("li");
+    highScoreEl.className = "high-score";
+    highScoreEl.innerHTML =
+      highScores[i].initials + " - " + highScores[i].score;
+    listHighScoreEl.appendChild(highScoreEl);
+  }
 
-    saveHighScore();
-    displayHighScores();
+  saveHighScore();
+  displayHighScores();
 };
 
 // Save High Score Function
 var saveHighScore = function () {
-    localStorage.setItem("HighScores", JSON.stringify(highScores));
+  localStorage.setItem("HighScores", JSON.stringify(highScores));
 };
 
 // Load High Score List Function
 var loadHighScore = function () {
-    var loadedHighScores = localStorage.getItem("HighScores");
-    if (!loadedHighScores) {
-        return false;
-    };
+  var loadedHighScores = localStorage.getItem("HighScores");
+  if (!loadedHighScores) {
+    return false;
+  }
 
-    loadedHighScores = JSON.parse(loadedHighScores);
-    loadedHighScores.sort((a, b) => { return b.score - a.score });
+  loadedHighScores = JSON.parse(loadedHighScores);
+  loadedHighScores.sort((a, b) => {
+    return b.score - a.score;
+  });
 
-    for (var i = 0; i < loadedHighScores.length; i++) {
-        var highScoreEl = document.createElement("li");
-        highScoreEl.className = "high-score";
-        highScoreEl.innerText = loadedHighScores[i].initials + " - " + loadedHighScores[i].score;
-        listHighScoreEl.appendChild(highScoreEl);
-        highScores.push(loadedHighScores[i]);
-    };
+  for (var i = 0; i < loadedHighScores.length; i++) {
+    var highScoreEl = document.createElement("li");
+    highScoreEl.className = "high-score";
+    highScoreEl.innerText =
+      loadedHighScores[i].initials + " - " + loadedHighScores[i].score;
+    listHighScoreEl.appendChild(highScoreEl);
+    highScores.push(loadedHighScores[i]);
+  }
 };
 
 // Display High Scores Function
 var displayHighScores = function () {
-    containerHighScoresEl.classList.remove("hide");
-    containerHighScoresEl.classList.add("show");
-    gameover = "true";
+  containerHighScoresEl.classList.remove("hide");
+  containerHighScoresEl.classList.add("show");
+  gameover = "true";
 
-    if (containerEndEl.className = "show") {
-        containerEndEl.classList.remove("show");
-        containerEndEl.classList.add("hide");
-    };
+  if ((containerEndEl.className = "show")) {
+    containerEndEl.classList.remove("show");
+    containerEndEl.classList.add("hide");
+  }
 
-    if (containerStartEl.className = "show") {
-        containerStartEl.classList.remove("show");
-        containerStartEl.classList.add("hide");
-    };
+  if ((containerStartEl.className = "show")) {
+    containerStartEl.classList.remove("show");
+    containerStartEl.classList.add("hide");
+  }
 
-    if (containerQuestionEl.className = "show") {
-        containerQuestionEl.classList.remove("show");
-        containerQuestionEl.classList.add("hide");
-    };
+  if ((containerQuestionEl.className = "show")) {
+    containerQuestionEl.classList.remove("show");
+    containerQuestionEl.classList.add("hide");
+  }
 
-    if (correctEl.className = "show") {
-        correctEl.classList.remove("show");
-        correctEl.classList.add("hide");
-    };
+  if ((correctEl.className = "show")) {
+    correctEl.classList.remove("show");
+    correctEl.classList.add("hide");
+  }
 
-    if (wrongEl.className = "show") {
-        wrongEl.classList.remove("show");
-        wrongEl.classList.add("hide");
-    };
+  if ((wrongEl.className = "show")) {
+    wrongEl.classList.remove("show");
+    wrongEl.classList.add("hide");
+  }
 };
 
 // Clear High Scores Function
 var clearScores = function () {
-    highScores = [];
+  highScores = [];
 
-    while (listHighScoreEl.firstChild) {
-        listHighScoreEl.removeChild(listHighScoreEl.firstChild);
-    };
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
 
-    localStorage.clear(highScores);
+  localStorage.clear(highScores);
 };
 
 loadHighScore();
